b4a74fb8bfead5f0c696fcfebdbfee4cde60f538
NotALobster <92136499+NotALobster@users.noreply.github.com>
2022-02-15 11:52:01 -0800

    Commented out something I forgot to comment out
    function was being defined twice.

--- sql_login.js
+++ sql_login.js
@@ -52,6 +52,7 @@ async function execute_query(sql_arg, var_args = null){
 }
 */
 
+/*
 async function execute_query(sql_arg, var_args = null){
     con.query(sql_arg, (error, results, fields) =>{
         if(error){
@@ -60,8 +61,7 @@ async function execute_query(sql_arg, var_args = null){
         return results;
     });
 }
-
-
+*/
 
 
 
