12dc8170d9fecd2791816ab3f1fce49aff44f0ae
John Tran <jtran4481@gmail.com>
2022-02-20 18:49:16 -0800

    Added Login/Register Box
    Purely frontend additions

--- client/src/components/LoginReg.js
+++ client/src/components/LoginReg.js
@@ -1,12 +1,12 @@
 import { Fragment, useState } from 'react';
 import logo from './../logo.svg';
-//import './FileUpload.css'
+import './LoginReg.css'
 import { Link} from "react-router-dom";
 import Axios from "axios";
 function LoginReg() {
 
 	const [username, setUsername] = useState('');
-	const [filename, setFilename] = useState('Choose File');
+	const [password, setPassword] = useState('');
 
 	const onChange = e => {
 		setUsername(e.target.value);
@@ -14,12 +14,12 @@ function LoginReg() {
 		console.log(username);
 	};
 
-	
+	//prob use https to send password or something idk
 	const login = () => {
 		
 		Axios.post("http://localhost:3305/login", {
 			username: username,
-			password: "Passwordle",
+			password: "Password",
 		}).then((response) => {
 			console.log(response);
 		});	
@@ -39,17 +39,24 @@ function LoginReg() {
 	
 	
 	return(
-		<div className="custom-file">
-			<div className="container">
+		<div className="login-box">
+			<div className="login-container">
 				<section className="cred">
-
-					<input type="text" onChange={onChange} />
-					<input type="text" onChange={onChange} />
-					<button onClick={login}> Login </button>
-					<button onClick={test}> test </button>
+					<h1>Logo< /h1>
+					<p>Username< /p>
+					<input className="input-area" type="text" name= "email" onChange={e => {setUsername(e.target.value);} } />
+					<p>Password< /p>
+					<input className="input-area" type="password" name= "password" onChange={e => {setPassword(e.target.value);} } />
+					<button className="input-btn" onClick={login}> Login </button>
+					<button className="input-btn" onClick={test}> Register </button>
 				</section>
 
 			</div>
+
+		</div>
+	);
+}
+			/* used to show username was grabbed
 			      {username &&
         <div className='row mt-5'>
           <div className='col-md-6 m-auto'>
@@ -57,10 +64,7 @@ function LoginReg() {
 					<p>{username}</p>
           </div>
         </div>
-      }
-		</div>
-	);
-}
+      }*/
 				/*
 				Add props that allow for resizing for dashboard
 				<img src={logo} className="App-logo" alt="logo" Style="width:10%" />*/
