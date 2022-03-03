014c0f64b30b2327c4e0216c24f49ab8ed434540
Jolin Zhang <jolin51502@gmail.com>
2022-03-03 14:59:49 -0800

    some visual updates to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,6 +20,17 @@ const Home = () => {
   console.log(current);
 
   return (
+    <>
+    <div style={{
+      backgroundImage: `url("https://images.unsplash.com/photo-1530569673472-307dc017a82d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1976&q=80")`,
+      backgroundRepeat: 'no-repeat',
+    }}>
+    <section className="heading">
+      Welcome to NFT Trading Simulator
+    </section>
+    <section className="nftbody">
+      Now supporting trading, buying NFTs, and viewing friends' profiles!
+    </section>
     <section className="slider">
       <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide}/>
       <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide}/>
@@ -32,6 +43,8 @@ const Home = () => {
         
       })}
     </section>
+    </div>
+    </>
   );
 };
 
