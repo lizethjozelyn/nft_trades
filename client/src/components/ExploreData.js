ebdee4d9753bae78a6ad4be15f73004764f28f32
lizethjozelyn <lizethjozelyn@g.ucla.edu>
2022-03-03 17:39:31 -0800

    store_page
    added files fetchimage and exploredata to show an explore page of images in store

---
+++ client/src/components/ExploreData.js
@@ -0,0 +1,17 @@
+export const ExploreData =[
+    {
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomicbook.com%2Fanime%2Fnews%2Fdemon-slayer-tanjiro-saves-nezuko-demon-blood-collection-tamayo-update%2F&psig=AOvVaw3RkiJwDdzLbvFqXKiD91q6&ust=1646436349067000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCm6cKLq_YCFQAAAAAdAAAAABAD'
+    },
+    {
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.xboxdynasty.de%2Fnews%2Fdemon-slayer-kimetsu-no-yaiba-hinokami-keppuutan%2Fzwei-audiooptionen-und-lokalisierte-untertitel-bestaetigt%2F&psig=AOvVaw3RkiJwDdzLbvFqXKiD91q6&ust=1646436349067000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCm6cKLq_YCFQAAAAAdAAAAABAT'
+    },    {
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.denofgeek.com%2Ftv%2Fdemon-slayer-season-2-ending-explained%2F&psig=AOvVaw3RkiJwDdzLbvFqXKiD91q6&ust=1646436349067000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCm6cKLq_YCFQAAAAAdAAAAABAw'
+    },
+    {
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Fpaultassi%2F2021%2F02%2F14%2Fdemon-slayer-season-2-gets-a-new-trailer-and-vague-release-date%2F&psig=AOvVaw3RkiJwDdzLbvFqXKiD91q6&ust=1646436349067000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLCm6cKLq_YCFQAAAAAdAAAAABAy'
+    },
+    {
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomicbook.com%2Fanime%2Fnews%2Fdemon-slayer-season-2-anime-review-entertainment-district-arc%2F&psig=AOvVaw0IgHysYQY41aRu7PUi8v1l&ust=1646439618389000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjYj9qXq_YCFQAAAAAdAAAAABAD'
+    }
+]
+
--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,15 +1,46 @@
 import logo from './../logo.svg';
 import Header from './../components/Header' 
 import Upload from './../components/FileUpload'
+import FetchImage from './../components/FetchImage'
+import ExploreData from './../components/ExploreData'
 
-function Store() {
-  return (
-    <div className="App">
-      <header className="">
-          <Upload/>
-      </header>
-    </div>
-  );
+function Store(){
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+    return(
+           <div class="row">
+             <div class="column">
+               <FetchImage(1)/>
+             </div>
+             <div class="column">
+                <FetchImage(2)/>
+             </div>
+             <div class="column">
+                <FetchImage(3)/>
+             </div>
+             <div class="column">
+                <FetchImage(4)/>
+             </div>
+           <div class="column">
+              <FetchImage(5)/>
+           </div>
+           </div>
+
+
+           <div class="container">
+             <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+
+   
+             <img id="expandedImg" style="width:100%">
+
+
+             <div id="imgtext"></div>
+           </div>
+           );
+   
 }
 
 export default Store;
