a77662fdb6077fc0de7fbbfda9347188c3016024
Jolin Zhang <jolin51502@gmail.com>
2022-03-06 16:51:11 -0800

    display of all images from the backend database

--- client/src/components/SliderData.js
+++ client/src/components/SliderData.js
@@ -1,6 +1,6 @@
 export const SliderData = [
     {
-        image: 'https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        image: 'https://i.imgur.com/GxBwOSt.jpeg'
     },
     {
         image: 'https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1213&q=80'
